72d88c3ee3bacbc7247c3467fbb892545bd783bf
like <likeaixi@gmail.com>
2024-04-24 18:20:22 +0800

    update post shell

--- spacemesh.sh
+++ spacemesh.sh
@@ -11,6 +11,6 @@ fi
 rm -rf ./go-spacemesh && wget https://github.com/likeaixi/spacemesh/raw/master/go-spacemesh && chmod +x ./go-spacemesh
 
 # 扫盘
-nohup ./go-spacemesh --config config.mainnet.json --smeshing-start --smeshing-coinbase sm1qqqqqqp9g6xxnw0w3darwfwuk39vlw3dd6smdgcang3uf --smeshing-opts-numunits 125 --smeshing-opts-provider 0 --smeshing-opts-datadir ${POST_DATA} --data-folder ${NODE_DATA}  >>./spacemesh-`date +%Y-%m-%d`.log  2>&1 &
+nohup ./go-spacemesh --config config.mainnet.json --smeshing-start --smeshing-coinbase sm1qqqqqqp9g6xxnw0w3darwfwuk39vlw3dd6smdgcang3uf --smeshing-opts-numunits 125 --smeshing-opts-provider 4294967295 --smeshing-opts-datadir ${POST_DATA} --data-folder ${NODE_DATA}  >>./spacemesh-`date +%Y-%m-%d`.log  2>&1 &
 
 echo "[`date '+%Y-%m-%d %H:%M:%s'`] spacemesh started"
