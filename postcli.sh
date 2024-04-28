48c306565fe891983df54a04b38fed1d29e33af8
like <likeaixi@gmail.com>
2024-04-28 17:52:58 +0800

    update postcli shell

--- postcli.sh
+++ postcli.sh
@@ -3,8 +3,9 @@
 i=1
 while [ $i -le 11 ]
 do
-/root/postcli -provider=0 -commitmentAtxId=0c545ed3ec10d97f1da60ca0197c69abcd46f259833a1bd5c5a71c2dac3cafbf -numUnits=125  -datadir=/data${i}/spacemesh/post_data
+nohup /root/postcli -provider=0 -commitmentAtxId=0c545ed3ec10d97f1da60ca0197c69abcd46f259833a1bd5c5a71c2dac3cafbf -numUnits=125  -datadir=/data${i}/spacemesh/post_data &
+sleep 3
 killall postcli
-
+sleep 3
 i=$((i+1))
 done
