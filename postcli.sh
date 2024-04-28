88ff06b1e79b03bd4c71c46ff6f03f94a47643ca
like <likeaixi@gmail.com>
2024-04-28 15:59:08 +0800

    update postcli init shell

--- postcli.sh
+++ postcli.sh
@@ -6,5 +6,5 @@ do
 /root/postcli -provider=0 -commitmentAtxId=0c545ed3ec10d97f1da60ca0197c69abcd46f259833a1bd5c5a71c2dac3cafbf -numUnits=125  -datadir=/data${i}/spacemesh/post_data
 killall postcli
 
-i=$(($i+1))
+i=$((i+1))
 done
